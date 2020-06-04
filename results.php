40be4c39abc66394a933f7cae35e98b7735867e8
vanderson19 <andersonvy13@gmail.com>
2020-06-04 12:21:59 -0700

    Update results.php

--- results.php
+++ results.php
@@ -1,5 +1,17 @@
 <html>
 <body>
 <h4>Birthday: <?php echo $_GET["m"]; ?> / <?php echo $_GET["d"]; ?> / <?php echo $_GET["y"]; ?></h4>
+<h2>Your fanny felicitation date is:</h2>
+<p>
+This makes your heinie <?php ;;;;;;;; ?> years old.<br>
+Your booty shares its formation day with <?php ;;;; ?>'s posterior.<br>
+Astrological sign: <?php ;;; ?>
+Your tush is <?php ;;; ?>
+<!-- three adjectives based on sign -->
+Gemstone: <?php ;;; ?>
+Your caboose will likely be a <?php ;;; ?>
+<!-- career article -->
+</p>
+
 </body>
 </html>
